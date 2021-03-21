b3298cee888a52f3d990bae1233f8fd8aedeab5f
Sergey <act1987@mail.ru>
2021-03-21 03:25:12 +0200

    Home Page project

--- src/blocks/burger-menu/burger-menu.js
+++ src/blocks/burger-menu/burger-menu.js
@@ -2,8 +2,8 @@ function mobileMenu() {
 
     let $burgerButton = document.querySelector('.burger-menu');
     let burgerButtonActive = 'burger-menu--active';
-    let $wrapperMenu = document.querySelector('.mobile-menu');
-    let wrapperMenuActive = 'mobile-menu--active';
+    let $wrapperMenu = document.querySelector('.header__menu');
+    let wrapperMenuActive = 'header__menu--active';
 
     $burgerButton.addEventListener('click', () => {
         $burgerButton.classList.toggle(burgerButtonActive);
